2edcbc453320b7b4f9601706ba6a0cf9540372ce
JannikWibker <JannikWibker@gmail.com>
2016-05-27 13:18:12 +0200

    #

--- js/sw.js
+++ js/sw.js
@@ -1,28 +1,32 @@
-this.addEventListener('install', (event) => {
+let CACHE_NAME = 'static-v1'
+let urlsToCache = [
+  '/calendar/',
+  '/calendar/js/app.js',
+  '/calendar/js/kalista.js',
+  '/calendar/css/style.css',
+  '/calendar/img/menu-title.jpeg',
+  '/calendar/img/icon-large.png',
+  '/calendar/img/icon.png',
+  '/calendar/img/icon-small.png'
+]
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.create('static-v1').then( (cache) => {
-      return cache.add([
-        '/calendar/',
-        '/calendar/js/app.js',
-        '/calendar/js/kalista.js',
-        '/calendar/css/style.css',
-        '/calendar/img/menu-title.jpeg',
-        '/calendar/img/icon-large.png',
-        '/calendar/img/icon.png',
-        '/calendar/img/icon-small.png'
-      ])
-    })
+    caches.open(CACHE_NAME)
+      .then( (cache) => {
+        return cache.addAll(urlsToCache)
+      })
   )
 })
 
-this.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).catch(() => {
-      return event.default()
-    }).catch( () => {
-      return caches.math(
-        '/calendar/fallback.json'
-      )
-    })
+    caches.match(event.request)
+      .then( (response) => {
+        if(response) {
+          return response
+        }
+        return fetch(event.request)
+      })
   )
 })
